64634705317e90e08bef6cff1b86ff2f5544c5bc
Salamon <rozvanys@gmail.com>
2026-04-20 01:43:20 +0200

    tenyleg teljes

--- Program.cs
+++ Program.cs
@@ -214,42 +214,7 @@ namespace Uveghaz
 				Console.ReadLine();
 			}
 			
-			//int szerepkor = int.Parse(Console.ReadLine());
 			
-			//if (szerepkor == 1)
-			//	{
-			//	 n9 Szerep váltás");
-			//}
-			//else if (szerepkor == 2) {
-			//	Console.WriteLine("Adja meg az elvégezni kívánt feladatot 1 ültetés \n2 kiszedés \n3 teljes nullázás \n4 lekérdezés kordináta alapján \n5 általános lekérdezés \n6 locsolás \n8 hazamenetel (következő nap) \n9 Szerep váltás");
-			//}
-			//else if (szerepkor == 4)
-			//{
-			//	Console.WriteLine("Adja meg az elvégezni kívánt feladatot \n4 lekérdezés kordináta alapján \n5 általános lekérdezés \n6 locsolás \n7 permetezés \n8 hazamenetel (következő nap) \n9 Szerep váltás");
-			//}else
-			//{
-			//	Console.WriteLine("Adja meg az elvégezni kívánt feladatot \n4 lekérdezés kordináta alapján \n5 általános lekérdezés \n7 locsolás \n8 hazamenetel (következő nap) \n9 Szerep váltás");
-			//}
-			//int feladat = int.Parse(Console.ReadLine());
-
-			//if (feladat == 1)
-			//{
-			//	Console.WriteLine("Adja meg hogy milyen növényt szeretne ültetni: ");
-			//	string novenyNev = Console.ReadLine();
-			//	Console.WriteLine("Adja meg hogy hányat szeretne ültetni: ");
-			//	int novenyMeny = int.Parse(Console.ReadLine());
-			//	Console.WriteLine("Adja meg hogy melyik oszlopba szeretné helyezni a növényt: ");
-			//	int novenyY = int.Parse(Console.ReadLine());
-			//	Console.WriteLine("Adja meg hogy melyik Sorba szeretné helyezni a növényt: ");
-			//	int novenyX = int.Parse(Console.ReadLine());
-				
-			//	NovenyFaj valasztottNoveny = new NovenyFaj(novenyNev, 8, false, 0);
-			//	racs.Telepit(novenyX, novenyY, valasztottNoveny, 5);
-			//	racs.TerepKiir();
-			//}
-			//if (feladat == 5) {
-			//	racs.TerepKiir();
-			//}
 		}
 	}
 }
